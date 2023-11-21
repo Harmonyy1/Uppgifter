4eabea23200b8d8ec46e979ee0b6cdb743b71a6a
Harmonyy1 <gsennerblom@gmail.com>
2023-11-21 11:52:19 +0100

    mycket bra

--- polymorfism.py
+++ polymorfism.py
@@ -28,12 +28,16 @@ class Kalkylator:
     def utför_operation(self, operation, num1, num2):
         return operation.utför(num1, num2)
     
-    def main():
-        addition = Addition()
-        subtraktion = Subtraktion()
-        multiplikation = Multiplikation()
-
-
-    def utför(self, num1, num2):
-        return num1 * num2
     
+def main():
+    addition = Addition()
+    subtraktion = Subtraktion()
+    multiplikation = Multiplikation()
+    kalkylator = Kalkylator()
+
+    print("Addition: ", kalkylator.utför_operation(addition, 10, 5))    #är "operation" bara en placeholder för där något av räknesätten ska sättas in?
+    print("Subtraktion: ", kalkylator.utför_operation(subtraktion, 10, 5))
+    print("Multiplikation: ", kalkylator.utför_operation(multiplikation, 10, 5))
+
+if __name__ == "__main__":
+    main()
