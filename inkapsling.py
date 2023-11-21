88c068aecd900ff14d57a88796c7811dfbdc56eb
Harmonyy1 <gsennerblom@gmail.com>
2023-11-21 23:43:19 +0100

    dfghjuiuifgh

--- inkapsling.py
+++ inkapsling.py
@@ -1,5 +1,5 @@
 #Uppgift 1
-
+"""
 class Bankkonto():
     def __init__(self, namn, saldo = 0):
         self.__namn = namn
@@ -24,14 +24,50 @@ konto = Bankkonto("Gojo Satoru", 7350)
 konto.sätta_in(1000)
 konto.ta_ut(1500)
 konto.visa_saldo()
-
+"""
 
 #Uppgift 2
 
-class Studerande():
+class Studerande:
     def __init__(self, namn, ålder, betyg):
         self.__namn = namn
-        self.__ålder = ålder
-        self.__betyg = betyg
+        self.__ålder = ålder if ålder > 0 else 18
+        self.__betyg = []
 
+    def lägga_till_betyg(self, betyg):
+        if 0 <= betyg <= 100:
+            self.__betyg.append(betyg)
+        else: 
+            print("ERROR - betyget måste vara mellan 1 - 100!")
+
+    def medelbetyg(self, betyg):
+        return sum(self.__betyg) / len(self.__betyg) if self.__betyg else 0
     
+    def sätt_info(self, namn, ålder):
+        self.__namn = namn
+        self.__ålder = ålder if ålder > 0 else self.__ålder
+
+    def få_info(self):
+        return self.__namn, self.__ålder, self.medelbetyg()
+    
+class Studerandehantering:
+    def __init__(self):
+        self.__studerandelista = []
+
+    def lägga_till_studerande(self, studerande):
+        self.__studerandelista.append(studerande)
+
+    def ta_bort_studerande(self, namn):
+	        self.__studerandelista = [s for s in self.__studerandelista if s.få_info()[0] != namn]
+
+
+
+hanterare = Studerandehantering()
+studerande1 = Studerande("Alice", 20, 78)
+studerande1.lägga_till_betyg(85)
+studerande1.lägga_till_betyg(90)
+hanterare.lägga_till_studerande(studerande1)
+studerande2 = Studerande("Bob", 22, 65)
+studerande2.lägga_till_betyg(92)
+studerande2.lägga_till_betyg(88)	
+hanterare.lägga_till_studerande(studerande2)
--- polymorfism.py
+++ polymorfism.py
@@ -1,43 +1,43 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod     #importerar för att kunna skapa abstrakta klasser
 
 
-class Operation(ABC):
+class Operation(ABC):   #basklass
+    @abstractmethod        #abstrakt metod
     def utför(self, num1, num2):
         pass
 
 
 
-class Addition(Operation):
-    
+class Addition(Operation):  #subklass
 
     def utför(self, num1, num2):
-        return num1 + num2
+        return num1 + num2      #returnerar värdet av argumenten efter addition
     
 class Subtraktion(Operation):
 
+    def utför(self, num1, num2):
+        return num1 - num2      #subtraktion
+    
+class Multiplikation(Operation):    
 
     def utför(self, num1, num2):
-        return num1 - num2
+        return num1 * num2      #multiplikation
     
-class Multiplikation(Operation):
-
-    def utför(self, num1, num2):
-        return num1 * num2
-    
-class Kalkylator:
+class Kalkylator:   #ny klass som använder operationklasserna
     def utför_operation(self, operation, num1, num2):
         return operation.utför(num1, num2)
     
     
-def main():
+def main():     #sätter operationklasserna under variabler
     addition = Addition()
     subtraktion = Subtraktion()
     multiplikation = Multiplikation()
     kalkylator = Kalkylator()
 
-    print("Addition: ", kalkylator.utför_operation(addition, 10, 5))    #är "operation" bara en placeholder för där något av räknesätten ska sättas in?
+    print("Addition: ", kalkylator.utför_operation(addition, 10, 5))    #anvädner metoden utför_operation där operation ersätts med en av operationsklasserna
     print("Subtraktion: ", kalkylator.utför_operation(subtraktion, 10, 5))
     print("Multiplikation: ", kalkylator.utför_operation(multiplikation, 10, 5))
 
+#kör huvudfunktionen
 if __name__ == "__main__":
     main()
