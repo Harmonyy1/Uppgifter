517f079ba81dfbe57daf054f0cb5af5214308f10
Harmonyy1 <gsennerblom@gmail.com>
2023-12-05 14:31:18 +0100

    fjbndfbndfug

---
+++ klassdiagram.py
@@ -0,0 +1,36 @@
+class Djur():
+    def __init__(self, namn):
+        self.namn = namn
+
+    def läte():
+        pass
+
+
+class Däggdjur(Djur):
+    def __init__(self, namn):
+        super.__init__(namn)
+        
+    def läte(self):
+        pass
+
+
+class Kräldjur(Djur):
+    def __init__(self, namn):
+        super.__init__(namn)
+    
+    def läte(self):
+        pass
+
+
+class Fåglar(Djur):
+    def __init__(self, namn):
+        super.__init__(namn)
+
+    def läte(self):
+        pass
+
+
+class 
+
+
+
