b57a967e5f5f5da62266d2f7d73613fe150d7c28
Harmonyy1 <gsennerblom@gmail.com>
2024-01-16 14:23:26 +0100

    help me

--- klassdiagram.py
+++ klassdiagram.py
@@ -2,13 +2,20 @@ class Djur():
     def __init__(self, namn):
         self.namn = namn
 
-    def läte():
+    def läte(self):
         pass
+
+    def __str__(self):
+        return f"{self.namn} låter såhär: {self.läte}"
+    
+
+        
+
 
 
 class Däggdjur(Djur):
     def __init__(self, namn):
-        super.__init__(namn)
+        super().__init__(namn)
         
     def läte(self):
         pass
@@ -16,7 +23,7 @@ class Däggdjur(Djur):
 
 class Kräldjur(Djur):
     def __init__(self, namn):
-        super.__init__(namn)
+        super().__init__(namn)
     
     def läte(self):
         pass
@@ -24,13 +31,33 @@ class Kräldjur(Djur):
 
 class Fåglar(Djur):
     def __init__(self, namn):
-        super.__init__(namn)
+        super().__init__(namn)
 
     def läte(self):
         pass
 
 
-class 
+
+class Människa(Däggdjur):
+    def __init__(self, namn):
+        super().__init__(namn)
+
+    def läte(self):
+        return f"Hey you! What are you doing in my house!?"
+    
+    def to_string(self):
+        return super().to_string()
+    
+    def print_info(self):
+        super().print_info()
+    
 
 
+hooman = Människa("Akile")
+
+hooman.print_info()
+
+
+
+#Kolla mer på __str__ och fixa så att koden fungerar
 
