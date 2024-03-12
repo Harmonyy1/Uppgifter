c213a1d8893d76ad18ac594dc8c3ad7359ebfd3d
Harmonyy1 <gsennerblom@gmail.com>
2024-03-12 14:20:51 +0100

    wtf

--- filhantering.py
+++ filhantering.py
@@ -1,7 +1,39 @@
+#Uppgift 1a
+"""
 while True:
+
     try:
         name = input("Filename: ")
-        file = open(name, "w")
-    except FileNotFoundError:
-        print("")
+        file = open(name, "r", encoding="utf-8")
+        reader = file.readlines()
+        info = [reader]
+        file.close()
+        break
+    except:
+        print("Gick inte att öppna filen")
 
+for read in reader:
+    print(read.strip())
+
+print("\nSlut på utskriften!\n")
+"""
+
+#Uppgift 1b
+
+while True:
+
+    try:
+        name = input("Filename: ")
+        file = open(name, "r", encoding="utf-8")
+        reader = file.readlines()
+        file.close()
+        break
+    except:
+        print("Gick inte att öppna filen")
+count = 1 
+for read in reader:
+    
+    print(str(count) + ".", str(read.strip()))
+    count = count + 1
+
+print("\nSlut på utskriften!\n")
